66d89520029588145aaa8e2a89f4fa04cc002fda
dekkard <agarciaf79@gmail.com>
2024-01-06 16:45:08 +0100

    Added duration to output list, changed separator to |

--- obtener_lista_ver_mas_tarde.py
+++ obtener_lista_ver_mas_tarde.py
@@ -4,9 +4,9 @@
 #
 ################################################
 
-
 import os
 import pickle
+import re
 import pandas as pd
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -15,6 +15,12 @@ from googleapiclient.discovery import build
 
 
 credentials = None
+
+def get_formatted_duration(duration):
+    """
+    Converts youtube duration format (PTxxHyyMzzS) to normal hour format (HH:MI:SS)
+    """
+    return(duration[2:].replace("H",":").replace("M",":").replace("S",""))
 
 # token.pickle stores the user's credentials from previously successful logins
 if os.path.exists('token.pickle'):
@@ -42,7 +48,7 @@ if not credentials or not credentials.valid:
             pickle.dump(credentials, f)
 
 
-print(credentials.to_json())
+# print(credentials.to_json())
 
 youtube = build ("youtube", "v3", credentials = credentials)
 # request = youtube.channels().list(part = "contentDetails", forUsername="dekkard79")
@@ -52,6 +58,7 @@ youtube = build ("youtube", "v3", credentials = credentials)
 
 #using nextToken to get pages
 playlist_items = []
+list_of_videos = []
 next_page_token = None
 
 while True:
@@ -61,30 +68,40 @@ while True:
         maxResults=50,  # Maximum number of results per page (you can adjust this)
         pageToken=next_page_token if next_page_token else ''
     )
-    response = request.execute()
-    playlist_items.extend(response['items'])
-    next_page_token = response.get('nextPageToken')
+    playlist_response = request.execute()
 
+    video_ids = [item["contentDetails"]["videoId"] for item in playlist_response['items']]
+    video_request = youtube.videos().list(
+        part="contentDetails",
+        id=','.join(video_ids)
+    )
+
+    video_response = video_request.execute()
+    playlist_items.extend(zip(playlist_response['items'], video_response['items']))
+    # playlist_items.extend(playlist_response['items'])
+
+
+    next_page_token = playlist_response.get('nextPageToken')
     if not next_page_token:
         break
 
-
-# print(response)
-# playlist_id de mis subidas: UU5VFigY67iOXHxtnaspn2Rg
-
-list_of_videos = []
-
+print(playlist_items[0:2])
 for item in playlist_items:
-    channel = item["snippet"]["videoOwnerChannelTitle"]
-    video_title = item["snippet"]["title"]
-    video_id = item["contentDetails"]["videoId"]
+    channel = item[0]["snippet"]["videoOwnerChannelTitle"]
+    video_title = item[0]["snippet"]["title"]
+    video_id = item[0]["contentDetails"]["videoId"]
+    video_duration = get_formatted_duration(item[1]["contentDetails"]["duration"])
     link = f"https://youtu.be/{video_id}"
     # print(f"Title: {playlist_title} | Id: {playlist_id}")
-    list_of_videos.append({"channel": channel, "video_title": video_title, "video_id":video_id, "link": link})
+    list_of_videos.append({"channel": channel, "video_title": video_title, "video_id":video_id, "duration":video_duration, "link": link})
     
 print(f"Total of {len(playlist_items)} items")
 df = pd.DataFrame(list_of_videos)
-print(df)
-df.to_csv('data.csv', index=False)
+print(df.head())
+column_names = ["CHANNEL", "VIDEO TITLE", "VIDEO ID", "VIDEO DURATION", "VIDEO LINK"]
+df.columns = column_names
+df.to_csv('data.csv', index=False, sep="|")
+# print(playlist_items[0]["snippet"])
+# print(playlist_items[0]["contentDetails"])
 
 
